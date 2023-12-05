db2a87860e85bb0e270b2d837f153ff3c70086db
Daniel Prendergast <danielpren@gmail.com>
2023-12-05 20:26:46 +0100

    Day05

---
+++ 2023/Day05/Challenge1/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Text.RegularExpressions;
+
+string strInput = File.ReadAllText("input.txt");
+long iLowestLocation = 0;
+
+string[] strGrouppedInput = strInput.Split(new string[] {"\r\n\r\n"},StringSplitOptions.None);
+
+string[] strSeeds = strGrouppedInput[0].Substring("seeds: ".Length).Split(" ");
+string[] strSeedToSoilMaps = strGrouppedInput[1].Substring("seed-to-soil map:: ".Length).Split("\r\n");
+string[] strSoilToFertilizerMaps = strGrouppedInput[2].Substring("soil-to-fertilizer map: \n".Length).Split("\r\n");
+string[] strFertilizerToWaterMaps = strGrouppedInput[3].Substring("fertilizer-to-water map: \n".Length).Split("\r\n");
+string[] strWaterToLightMaps = strGrouppedInput[4].Substring("water-to-light map: \n".Length).Split("\r\n");
+string[] strLightToTemperatureMaps = strGrouppedInput[5].Substring("light-to-temperature map: \n".Length).Split("\r\n");
+string[] strTemperatureToHumidityMaps = strGrouppedInput[6].Substring("temperature-to-humidity map: \n".Length).Split("\r\n");
+string[] strHumidityToLocationMaps = strGrouppedInput[7].Substring("humidity-to-location map: \n".Length).Split("\r\n");
+
+foreach (string strSeed in strSeeds)
+{
+    long iSeed = long.Parse(strSeed);
+    long iSoil = 0;
+    long iFertilizer = 0;
+    long iWater= 0;
+    long iLight= 0;
+    long iTemperature = 0;
+    long iHumidity = 0;
+    long iLocation = 0;
+
+
+    // Seed to Soil
+    foreach (string strMap in strSeedToSoilMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iSeed >= long.Parse(strRelations[1]) && iSeed < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iSoil = iSeed - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iSoil == 0)
+    {
+        iSoil = iSeed;
+    }
+
+    // Soil to Fertilizer
+    foreach (string strMap in strSoilToFertilizerMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iSoil >= long.Parse(strRelations[1]) && iSoil < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iFertilizer = iSoil - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iFertilizer == 0)
+    {
+        iFertilizer = iSoil;
+    }
+
+    // Fertilizer to Water
+    foreach (string strMap in strFertilizerToWaterMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iFertilizer >= long.Parse(strRelations[1]) && iFertilizer < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iWater = iFertilizer - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iWater == 0)
+    {
+        iWater = iFertilizer;
+    }
+
+    // Water to Light
+    foreach (string strMap in strWaterToLightMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iWater >= long.Parse(strRelations[1]) && iWater < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iLight = iWater - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iLight == 0)
+    {
+        iLight = iWater;
+    }
+
+    // Light to Temperature
+    foreach (string strMap in strLightToTemperatureMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iLight >= long.Parse(strRelations[1]) && iLight < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iTemperature = iLight - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iTemperature == 0)
+    {
+        iTemperature = iLight;
+    }
+
+    // Temperature to Humidity
+    foreach (string strMap in strTemperatureToHumidityMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iTemperature >= long.Parse(strRelations[1]) && iTemperature < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iHumidity = iTemperature - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iHumidity == 0)
+    {
+        iHumidity = iTemperature;
+    }
+
+    // Humidity to Location
+    foreach (string strMap in strHumidityToLocationMaps)
+    {
+        string[] strRelations = strMap.Split(" ");
+        if (iHumidity >= long.Parse(strRelations[1]) && iHumidity < long.Parse(strRelations[1]) + long.Parse(strRelations[2]))
+        {
+            iLocation = iHumidity - long.Parse(strRelations[1]) + long.Parse(strRelations[0]);
+        }
+    }
+    if (iLocation == 0)
+    {
+        iLocation = iHumidity;
+    }
+
+    if (iLocation < iLowestLocation || iLowestLocation == 0)
+    {
+        iLowestLocation = iLocation;
+    }
+}
+
+
+Console.WriteLine(iLowestLocation.ToString());
---
+++ 2023/Day05/Challenge2/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+string strInput = File.ReadAllText("input.txt");
+long iLowestLocation = 0;
+
+string[] strGrouppedInput = strInput.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+
+string[] strSeeds = strGrouppedInput[0].Substring("seeds: ".Length).Split(" ");
+string[] strSeedToSoilMaps = strGrouppedInput[1].Substring("seed-to-soil map:: ".Length).Split("\r\n");
+string[] strSoilToFertilizerMaps = strGrouppedInput[2].Substring("soil-to-fertilizer map: \n".Length).Split("\r\n");
+string[] strFertilizerToWaterMaps = strGrouppedInput[3].Substring("fertilizer-to-water map: \n".Length).Split("\r\n");
+string[] strWaterToLightMaps = strGrouppedInput[4].Substring("water-to-light map: \n".Length).Split("\r\n");
+string[] strLightToTemperatureMaps = strGrouppedInput[5].Substring("light-to-temperature map: \n".Length).Split("\r\n");
+string[] strTemperatureToHumidityMaps = strGrouppedInput[6].Substring("temperature-to-humidity map: \n".Length).Split("\r\n");
+string[] strHumidityToLocationMaps = strGrouppedInput[7].Substring("humidity-to-location map: \n".Length).Split("\r\n");
+
+int iAlternate = 0;
+long iBaseSeed = 0;
+
+//Create seed pairs
+var listPairs = new List<Tuple<long, long>>();
+foreach (string strSeed in strSeeds)
+{
+    if (iAlternate == 0)
+    {
+        iBaseSeed = long.Parse(strSeed);
+        iAlternate = 1;
+    }
+    else
+    {
+        listPairs.Add(new Tuple<long, long>(iBaseSeed, long.Parse(strSeed)));
+        iAlternate = 0;
+    }
+}
+
+listPairs.Sort((x, y) => y.Item1.CompareTo(x.Item1));
+listPairs.Reverse();
+
+int iRuns = 0;
+List<long> strSeedsRanged = new List<long>();
+
+foreach (Tuple<long, long> tuplePair in listPairs)
+{
+    long lSeedToAdd = tuplePair.Item1;
+    long lMax = tuplePair.Item1 + tuplePair.Item2;
+    long lTotalRuns = tuplePair.Item2;
+    try
+    {
+        //Thought this would help, but does absolutely nothing
+        if (lMax >= listPairs[iRuns + 1].Item1)
+        {
+            lTotalRuns = listPairs[iRuns + 1].Item1 - listPairs[iRuns].Item1;
+        }
+    }
+    catch { }
+    for (long l = 0; l < lTotalRuns; l++)
+    {
+        strSeedsRanged.Add(lSeedToAdd + l);
+    }
+    iRuns++;
+}
+
+iRuns = 1;
+
+
+//Precreate relations
+
+var listSeedToSoilRelations = CreateRelations(strSeedToSoilMaps);
+var listSoilToFertilizerRelations = CreateRelations(strSoilToFertilizerMaps);
+var listFertilizerToWaterRelations = CreateRelations(strFertilizerToWaterMaps);
+var listWaterToLightRelations = CreateRelations(strWaterToLightMaps);
+var listLightToTemperatureRelations = CreateRelations(strLightToTemperatureMaps);
+var listTemperatureToHumidityRelations = CreateRelations(strTemperatureToHumidityMaps);
+var listHumidityToLocationRelations = CreateRelations(strHumidityToLocationMaps);
+
+Console.Write("Running for :" + strSeedsRanged.Count + " seeds.");
+Console.WriteLine();
+
+foreach (long lSeed in strSeedsRanged)
+{
+    long iSeed = lSeed;
+    long iSoil = CheckArray(listSeedToSoilRelations, iSeed); ;
+    long iFertilizer = CheckArray(listSoilToFertilizerRelations, iSoil);
+    long iWater = CheckArray(listFertilizerToWaterRelations, iFertilizer); ;
+    long iLight = CheckArray(listWaterToLightRelations, iWater); ;
+    long iTemperature = CheckArray(listLightToTemperatureRelations, iLight); ;
+    long iHumidity = CheckArray(listTemperatureToHumidityRelations, iTemperature); ;
+    long iLocation = CheckArray(listHumidityToLocationRelations, iHumidity); ;
+
+    if (iLocation < iLowestLocation || iLowestLocation == 0)
+    {
+        iLowestLocation = iLocation;
+    }
+    iRuns++;
+    if (iRuns % 2000000 == 0)
+    {
+        double dPercent = (double)iRuns / (double)strSeedsRanged.Count;
+        dPercent = dPercent * 100;
+        Console.WriteLine(dPercent.ToString() + "%");
+    }
+}
+
+Console.WriteLine("Lowest Location Seed is: " + iLowestLocation.ToString());
+// Welp this pulls out the value +1, so the solution is actually incomplete but I want to get ready for day 6 :)
+Console.ReadKey(true);
+
+static long CheckArray(List<Tuple<long, long, long>> listRelations, long iSource)
+{
+    long lValue = 0;
+    foreach (Tuple<long, long, long> tuplePair in listRelations)
+    {
+        if (iSource >= tuplePair.Item2 && iSource < tuplePair.Item2 + tuplePair.Item3)
+        {
+            lValue = iSource - tuplePair.Item2 + tuplePair.Item1;
+        }
+    }
+    if (lValue == 0)
+    {
+        lValue = iSource;
+    }
+    return lValue;
+}
+
+static List<Tuple<long, long, long>> CreateRelations(string[] strStringToMap)
+{
+    var listRelations = new List<Tuple<long, long, long>>();
+    foreach (string strMap in strStringToMap)
+    {
+        string[] strRelations = strMap.Split(" ");
+        listRelations.Add(new Tuple<long, long, long>(long.Parse(strRelations[0]), long.Parse(strRelations[1]), long.Parse(strRelations[2])));
+    }
+    return listRelations;
+}
