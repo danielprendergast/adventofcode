156d461dc2e7d8a91ae65dcbbf0d2a08f7676090
Daniel Prendergast <danielpren@gmail.com>
2023-12-06 10:18:05 +0100

    Day06

---
+++ 2023/Day06/Challenge2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+string[] strInput = File.ReadAllLines("input.txt");
+
+string strNumberPattern = @"([0-9])+";
+Regex rExp = new Regex(strNumberPattern);
+
+MatchCollection matchTimes = rExp.Matches(strInput[0].ToString().Replace(" ",""));
+MatchCollection matchDistances = rExp.Matches(strInput[1].ToString().Replace(" ", ""));
+
+var listPairs = new List<Tuple<long, long>>();
+
+int iLoop = 0;
+foreach (Match match in matchTimes)
+{
+    listPairs.Add(new Tuple<long, long>(long.Parse(match.Value), long.Parse(matchDistances[iLoop].Value)));
+    iLoop++;
+}
+Console.WriteLine("");
+
+int iTotal = 0;
+
+foreach (Tuple<long, long> tuplePair in listPairs)
+{
+    int iSuccess = 0;
+    long iTime = tuplePair.Item1;
+    long iDistance = tuplePair.Item2;
+
+    for (int i = 0; i < iTime; i++)
+    {
+        int iSpeed = i;
+        long iCalculatedDistance = iSpeed * (iTime - iSpeed);
+
+        if (iCalculatedDistance > iDistance)
+        {
+            iSuccess++;
+        }
+    }
+
+    if (iTotal == 0)
+    {
+        iTotal = iSuccess;
+    }
+    else
+    {
+        iTotal = iTotal * iSuccess;
+    }
+
+}
+Console.WriteLine(iTotal.ToString());
